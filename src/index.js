ff0f1be19cd1957e123c9d979bcd87d17592f0f1
douy <douy@cs.washington.edu>
2020-02-11 23:50:09 -0800

    add play pause button and year slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-d3.json("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-game-dev-pubg/master/src/person_fixed_with_score.json").then( dat => {
+$(document).ready(function() {
+    var btn = $(".button");
+    btn.click(function() {
+      btn.toggleClass("paused");
+      return false;
+    });
+  });
+
+var rangeSlider = document.getElementById("rs-range-line");
+var rangeBullet = document.getElementById("rs-bullet");
+
+
+d3.json("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-game-dev-pubg/master/src/game_data.json").then( dat => {
     function draw() {
         // define the svg
         d3.selectAll("svg > *").remove();
@@ -23,21 +35,23 @@ d3.json("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-game-dev-pubg/maste
         // load labels and rects to svg
         let labels = groups.append("text").text(d => d.publisher).attr("id", "label").attr("x", rect.marginH).style("font-size", `${font.height}px`).on("click", function (d) {
             stop = true;
+            clearInterval(intervalId);
             d3.selectAll("svg > *").remove();
             svg = d3.select("svg").append("g");
             dataEntry = d;
-            dataValue = dataEntry["game"].sort((x, y) => y.user_score - x.user_score);
-            maxSale = dataValue[0].user_score;
+            dataValue = dataEntry["game"].sort((x, y) => y.game_global_sale - x.game_global_sale);
+            maxSale = dataValue[0].game_global_sale;
             maxHeight = (rect.marginV + rect.height) * (dataValue.length - 1) + rect.marginT;
             groups = svg.selectAll("g").data(dataValue).enter().append("g").style("cursor", "pointer");
             let gamelabels = groups.append("text").text(d => d.game_name).attr("x", rect.marginH).style("font-size", `${font.height}px`).on("click", () => draw());
             let rects = groups.append("rect").attr("x", rect.marginH + font.margin).attr("height", rect.height);
             let scale = d3.scaleLinear().domain([0, maxSale]).range([0, 300]);
             gamelabels.data(dataValue, d => d.game_name).transition().duration(600).attr("y", (_, i) => (rect.marginV + rect.height) * i + rect.marginT + rect.height / 2);
-            rects.data(dataValue, d => d.game_name).transition().duration(600).attr("y", (_, i) => (rect.marginV + rect.height) * i + rect.marginT).attr("width", d => scale(d.user_score));
+            rects.data(dataValue, d => d.game_name).transition().duration(600).attr("y", (_, i) => (rect.marginV + rect.height) * i + rect.marginT).attr("width", d => scale(d.game_global_sale));
             let xScale = d3.scaleLinear().domain([0, maxSale]).range([0, 300]);
-            let xAxis = d3.axisBottom(xScale).ticks(10).tickFormat(d3.format("d"));
+            let xAxis = d3.axisBottom(xScale).ticks(10).tickFormat(d3.format(".1f"));
             svg.append("g").attr("transform", "translate(" + (rect.marginH + font.margin) + "," + (maxHeight + rect.height + rect.marginV) + ")").call(xAxis);
+            d3.select("#play_pause_button").classed("paused", false)
         });
 
         let rects = groups.append("rect").attr("id", "rect").attr("x", rect.marginH + font.margin).attr("height", rect.height);
@@ -76,6 +90,7 @@ d3.json("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-game-dev-pubg/maste
             updateElements("total_global_sale");
 
         function update(i, type) {
+            document.getElementById("year_slider").value = i % dat.length + 2003;
             if (!stop) {
                 // update the data and year
                 dataEntry = dat[i];
@@ -105,16 +120,34 @@ d3.json("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-game-dev-pubg/maste
 
         function updateGraphType(type) {
             let index = 0;
-            intervalId = setInterval(() => update((index++) % dat.length, type), 1300);
+            intervalId = setInterval(() => update((index++) % dat.length, type), 700);
         }
 
         // radio button are selected
         d3.selectAll("input[name='type']").on("change", function () {
             clearInterval(intervalId);
+            d3.select("#play_pause_button").classed("paused", false)
             updateGraphType(this.value);
         });
 
-        intervalId = setInterval(() => update((++index) % dat.length, "total_global_sale"), 1300);
+        d3.select("#play_pause_button").on("click", function() {
+            if (d3.select("#play_pause_button").classed("paused")) {
+                for (var i = 1; i <= intervalId; i++)
+                    clearInterval(i);
+            } else {
+                intervalId = setInterval(() => update((index++) % dat.length,  d3.select("input[name='type']:checked").node().value), 700);
+            }
+        });
+
+        d3.select("#year_slider").on("input", function() {
+            d3.select("#play_pause_button").classed("paused", true)
+            clearInterval(intervalId);
+            index = this.value - 2003
+            update(index % dat.length,  d3.select("input[name='type']:checked").node().value)
+        });
+
+        intervalId = setInterval(() => update((++index) % dat.length, "total_global_sale"), 700);
     }
     draw();
 });
+
